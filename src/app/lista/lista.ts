88dbd3eba00e5363ef88b1d99512adeef200e989
Bianca Brandão <bianca.brandao.bbs@gmail.com>
2026-08-18 11:49:54 -0300

    feat: Carrossel criado

--- src/app/lista/lista.ts
+++ src/app/lista/lista.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import { GameService } from '../services/game';
 
 @Component({
   selector: 'app-lista',
   imports: [],
   templateUrl: './lista.html',
   styleUrl: './lista.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Lista {
+  private readonly gameService = inject(GameService);
+
+  jogos = computed(() => [...this.gameService.games()].sort((a, b) => b.id - a.id));
 
 }
