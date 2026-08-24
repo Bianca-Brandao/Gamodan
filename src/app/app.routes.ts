296d0c030d55e1be7ee65eeb6026dee99e26155f
Bianca Brandão <bianca.brandao.bbs@gmail.com>
2026-08-24 11:54:55 -0300

    Fix Dup Errors

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,18 +4,11 @@ import { Lista } from './lista/lista';
 import { Contato } from './contato/contato';
 import { Admin } from './features/admin/admin';
 import { authGuard } from './services/auth-guard';
+import { Perfil } from './perfil/perfil';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'admin', component: Admin, canActivate: [authGuard]},
-  { path: 'lista', component: Lista},
-  { path: 'favoritos', component: Favoritos },
-  { path: 'status', component: Status },
-import { Perfil } from './perfil/perfil';
-import { authGuard } from './auth.guard';
-
-export const routes: Routes = [
-  { path: '', component: Home },
   { path: 'lista', component: Lista, canActivate: [authGuard] },
   { path: 'perfil', component: Perfil, canActivate: [authGuard] },
   { path: 'contato', component: Contato },
