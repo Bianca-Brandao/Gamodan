26f2be69bab1f15c5ca9ead3acc22fe294e1f60f
Bianca Brandão <bianca.brandao.bbs@gmail.com>
2026-08-24 11:36:34 -0300

    Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Lista } from './lista/lista';
-import { Favoritos } from './favoritos/favoritos';
-import { Status } from './status/status';
 import { Contato } from './contato/contato';
 import { Perfil } from './perfil/perfil';
 import { authGuard } from './auth.guard';
@@ -10,8 +8,6 @@ import { authGuard } from './auth.guard';
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'lista', component: Lista, canActivate: [authGuard] },
-  { path: 'favoritos', component: Favoritos, canActivate: [authGuard] },
-  { path: 'status', component: Status, canActivate: [authGuard] },
   { path: 'perfil', component: Perfil, canActivate: [authGuard] },
   { path: 'contato', component: Contato },
 ];
